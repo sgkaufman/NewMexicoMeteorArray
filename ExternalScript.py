66046e6f249f3e4f3ddbbc61457c2072f7b5c524
Stephen G Kaufman <stephengkaufman@gmail.com>
2021-02-08 19:37:41 +0000

    Add TimeLapse.sh and ExternalScript.py
    Initial Commit of TimeLapse.sh and ExternalScript.py.
        TimeLapse.sh copies radiants.txt, and .csv files to RMS_data/csv/,
        then creates a TimeLapse.mp4 file, and stack of all captured images,
        which are moved to RMS_data. The first argument is the subdirectory
        of CapturedFiles which is to be processed. Creation of the mp4
        can be suppressed by supplying "0" (zero), "No", or "False"
        for the second argument. Creation of the stack of captured FITS files
        can be suppressed by supplying "0" (zero), "No", or "False" for the
        third argument.
        TimeLapse.mp4, Radiants, Stack and Capture jpg files can optionally
        be copied to My_Uploads by setting "My_Uploads=1" in this script file.
        Default actions of this script can be changed by editing the
        values for the variables "TimeLapse" and "CapStack" in this script file.
        
        ExternalScript.py is called by RMS when the RMS ".config" file is so set.
        It uploads files to NMMA Server, creating log files, appending diagnostic
        information about the night's data collection to the
        "<station_name>_fits_count.txt" file (under /home/pi/RMS_Data/csv),
        calls TimeLapse.sh, and reboots. It can be called from the command line
        via "python -m RMS.ExternalScript --directory <subdirectory name> \
                --CreateTimeLapse [0,1] --CreateCaptureFile [0,1] \
                --reboot [0,1]
        In each case for the last 3 arguments, 0 means "do not",
        and "1" means "do" the action indicated by the option name.

---
+++ ExternalScript.py
@@ -0,0 +1,355 @@
+#!/usr/bin/env python
+
+"""
+This is Version 0.6 of file ExternalScript.py. Dated 02/01/2021.
+SGK 02/01/2021: Added support for creating a stack of the CapturedFiles
+                FITS files (argument --CreateCaptureStack). When 0 (zero),
+                a capture stack is not created. When "1", it is.
+                Defaults to "1".
+SGK 06/14/2020: Added Bluehost server 69.195.111.36 (dedicated IP)
+SGK 05/15/2020: Added computation of RMS.CaptureDuration for the night's
+                start time and capture duration, needed for the FITS file
+                creation watchdog.
+SGK 03/22/2020: Added creation of a log file for the shell scripts
+SGK 12/17/2019: 1. Added arguments "reboot" and "CreateTimeLapse"
+                to uploadFiles and to the "__main__" call of ExternalScript.py.
+                Both default to True. If reboot is True, RMS reboots at end of
+                ExternalScript processing. Otherwise it does not.
+                If CreateTimeLapse is True, the Timelapse.mp4 is created.
+                Otherwise it is not.
+                2. Normalized the syntax for determining length of lists in
+                the function getFilesAndUpload.
+                3. Added test for existence for file "Extra_Uploads.sh".
+                If it exists, it gets executed.
+
+This is Version 0.2 of file ExternalScript.py. Dated 12/08/2019.
+PNE 12/08/2019: Added section at line 180 with call to BackupToUSB.sh
+
+This is Version 0.1 of file ExternalScript.py. Dated 09/12/2019.
+SGK 09/12/2019: Put all file finding and uploading into function
+                 getFilesAndUpload(), and added fits_count.txt to upload.
+SGK 09/09/2019: Call "sudo -r now" at the end of the script.
+                Eliminate creation and removal of .reboot_lock file.
+                Assumes that "reboot_after_processing" in .config is false.
+SGK 09/07/2019: Made creation of the .reboot_lock file the first thing done
+                when uploadFiles is called.
+This script 
+1: Moves and/or copies files on the RMS stations, and 
+2: Uploads files to the New Mexico Meteor Array Server.
+"""
+from __future__ import print_function
+
+import os
+import sys
+import copy
+import logging
+import fnmatch
+import argparse
+import subprocess
+import time
+import datetime
+
+from RMS.CaptureDuration import captureDuration
+import RMS.ConfigReader
+from RMS.UploadManager import UploadManager
+from RMS.Logger import initLogging
+import ftplib
+from ftplib import FTP_TLS
+
+def makeLogFile(log_file_dir, prefix, time_arg=None):
+    
+    # Create a log file to provide as stdout and stderr for
+    # the subprocess.calls of the shell scripts.
+    # Return the file pointer to the log file in OPEN state.
+    # time_arg, if given, is a date string of the form
+    # "2020-05-20 02:33:45.123456". Cannot include a timezone at the end.
+
+    if time_arg is None:
+        time_to_use = datetime.datetime.utcnow()
+    else:
+        time_to_use = datetime.datetime.fromisoformat(time_arg)
+
+    log_filename = prefix + "_{0}_{1}_{2}".format \
+        (time_to_use.year, \
+         time_to_use.month, \
+         time_to_use.day)
+    full_filename = log_file_dir + log_filename
+    print("Creating log file name %s\n" % full_filename)
+    return full_filename
+
+########################################################################
+    
+def findFiles(dataDir, allFiles, pattern):
+    """
+    Searches allFiles for those matching the pattern.
+    allFiles is the list of files under dataDir,
+    and dataDir is added to the names of the files matching pattern.
+    Hence the list returned has all absolute path names.
+    """
+    found_files = []
+    pattern_files = fnmatch.filter(allFiles, pattern)
+    for pattern_file in pattern_files:
+        full_file = os.path.join(dataDir, pattern_file)
+        found_files.append(full_file)
+    return found_files
+
+def getFilesAndUpload(logger, nm_config, main_data_dir, log_file_fd):
+
+    # The argument "log_file_fd" is assumed to be passed in
+    # open state, and need not be closed during the call.
+    
+    logger.info('Starting the NM upload manager ...')
+    
+    # create the upload manager for the local files
+    upload_manager = UploadManager(nm_config)
+    upload_manager.start()
+
+    # Get the files from the main_data_dir for NM upload
+
+    all_files = os.listdir(main_data_dir)
+
+    png_files = findFiles(main_data_dir, all_files, "*.png")
+    print("Adding %d png files to queue ..." % len(png_files), file=log_file_fd)
+    upload_manager.addFiles(png_files)
+    
+    jpg_files = findFiles(main_data_dir, all_files, "*.jpg")
+    print("Adding %d jpg files to queue ..." % len(jpg_files), file=log_file_fd)   
+    upload_manager.addFiles(jpg_files)
+
+    bmp_files = findFiles(main_data_dir, all_files, "*.bmp")
+    print("Adding %d bmp files to queue ..." % len(bmp_files), file=log_file_fd)   
+    upload_manager.addFiles(bmp_files)
+    
+    #ftp_files = findFiles(main_data_dir, all_files, "FTP*")
+    #print("Adding %d ftp files to queue ..." % ftp_files.__len__())
+    #upload_manager.addFiles(ftp_files)
+    
+    csv_files = findFiles(main_data_dir, all_files, "*.csv")
+    print("Adding %d csv files to queue ..." % len(csv_files), file=log_file_fd)
+    upload_manager.addFiles(csv_files)
+    
+    cal_files = findFiles(main_data_dir, all_files, "*.cal")
+    print("Adding %d cal files to queue ..." % len(cal_files), file=log_file_fd)
+    upload_manager.addFiles(cal_files)
+    
+    txt_files = findFiles(main_data_dir, all_files, "*.txt")
+    print("Adding %d txt files to queue ..." % len(txt_files), file=log_file_fd)
+    upload_manager.addFiles(txt_files)
+    
+    #platepar_files = findFiles(main_data_dir, all_files, "platepar*.cal")
+    #print("Adding %d platepar files to queue ..." % len(platepar_files), file=log_file_fd)
+    #upload_manager.addFiles(platepar_files)
+
+    config_files = findFiles(main_data_dir, all_files, "/home/pi/source/RMS/.config")
+    print("Adding %d config files to queue ..." % len(config_files), file=log_file_fd)    
+    upload_manager.addFiles(config_files)
+
+    csv_dir = os.path.join(nm_config.data_dir, 'csv/')
+    print("csv_dir set to %s" % csv_dir, file=log_file_fd)
+
+    fits_count_file = findFiles(csv_dir, os.listdir(csv_dir), \
+                               "*fits_counts.txt")
+    print("Adding %d fits_count.txt files to queue ..." % len(fits_count_file), \
+          file=log_file_fd)
+    upload_manager.addFiles(fits_count_file)
+    
+    # Get the .config file if the calibration file does not exist?
+
+    # Begin the upload!
+    
+    upload_manager.uploadData()
+
+    upload_manager.stop()
+
+
+def uploadFiles(captured_night_dir, archived_night_dir, config, log_upload=True, reboot=True, CreateTimeLapse=True, CreateCaptureStack=True, which='A', preset='micro'):
+    """ Function to upload selected files from the ArchivedData or CapturedData
+        directory to the New_Mexico_Server.
+        Files to transfer include:
+        *.jpg
+        FTP*.txt
+        *.png
+        *.csv
+        *.cal
+        platepar*.cal
+        .config (in case no platepar_cmn2010.cal)
+    """
+
+    # Variable definitions
+    if which == 'C':
+        main_data_dir = captured_night_dir
+    else:
+        main_data_dir = archived_night_dir
+    extra_uploads_file = "/home/pi/source/RMS/Extra_Uploads.sh"
+    remote_dir = '/Users/meteorstations/Public'
+
+    RMS_data_dir_name = os.path.abspath("/home/pi/RMS_data/")
+    print ("RMS_data_dir_name = {0}".format(RMS_data_dir_name))
+    data_dir_name = os.path.basename(main_data_dir)
+    print ("data_dir_name = {0}".format(data_dir_name))
+    log_dir_name = os.path.join(RMS_data_dir_name, 'logs/')
+    print ("log_dir_name = {0}".format(log_dir_name))
+
+    # Create the config object for New Mexico Meteor Array purposes
+    nm_config = copy.copy(config)
+    nm_config.stationID = 'meteorstations'
+    nm_config.hostname = '10.8.0.61' # 69.195.111.36 for Bluehost
+    nm_config.remote_dir = remote_dir
+    nm_config.upload_queue_file = 'NM_FILES_TO_UPLOAD.inf'
+    # nm_config.data_dir = os.path.join(os.path.expanduser('~'), 'NM_data')
+    # nm_config.log_dir = os.path.join(os.path.expanduser('~'), 'NM_data/logs')
+
+    # Compute and write out the next start time and capture time
+    start_time, duration = captureDuration(config.latitude, \
+                                           config.longitude, \
+                                           config.elevation)
+    duration_int = round(duration)
+    time_str = start_time.isoformat(' ')
+    print ("Time string is: %s" % time_str)
+    time_file = makeLogFile(log_dir_name, "CaptureTimes", time_str)
+
+    with open(time_file, 'w') as time_fd:
+        print(time_str, file=time_fd)
+        print(duration_int, file=time_fd)
+
+    # Make and save the file descriptor for shell script and print function
+    # calls. The "w+" mode ensures that the files is created if necessary.
+    # The file remains open for writing until its "closed" method is called.
+
+    log_file_name = makeLogFile(log_dir_name, "ShellScriptLog")
+    with open(log_file_name, 'w+') as log_file:
+        # Print out the arguments and variables of interest
+        print("remote_dir set to %s" % remote_dir, file=log_file)
+        print("Name of program running = %s" % (__name__), file=log_file)
+        print("reboot arg = %s" % reboot, file=log_file)
+        print("CreateTimeLapse arg = %s" % CreateTimeLapse, file=log_file)
+        print("log_dir_name = %s" % log_dir_name, file=log_file)
+        print("ArchivedFiles directory = %s" % archived_night_dir, \
+              file=log_file)
+
+        # What is it about subprocess.call, with shell=True, that makes
+        # StartCapture execute? Or, at least some update checking
+        # take place?
+
+        # Prepare for calls to TimeLapse.sh,
+        # second arg based on CreateTimeLapse,
+        # third arg based on CreateCaptureStack
+        TimeLapse_cmd_str = "/home/pi/source/RMS/TimeLapse.sh " + data_dir_name
+        if  CreateTimeLapse:
+            TimeLapse_cmd_str = TimeLapse_cmd_str + " Yes"
+        else:
+            TimeLapse_cmd_str = TimeLapse_cmd_str + " No"
+
+        if CreateCaptureStack:
+            TimeLapse_cmd_str = TimeLapse_cmd_str + " Yes"
+        else:
+            TimeLapse_cmd_str = TimeLapse_cmd_str + " No"
+            
+        print("TimeLapse_cmd_str = ", TimeLapse_cmd_str, file=log_file)
+        status = subprocess.call(TimeLapse_cmd_str, \
+                                 stdout=log_file, \
+                                 stderr=log_file, \
+                                 shell=True)
+        print("TimeLapse call returned with status ", \
+              status, file=log_file)
+    
+        # backup data to thumb drive, PNE 12/08/2019
+        status = subprocess.call("/home/pi/source/RMS/BackupToUSB.sh " \
+                                 + data_dir_name, \
+                                 stdout=log_file, \
+                                 stderr=log_file, \
+                                 shell=True)
+        print("BackupToUSB call returned with status ", \
+              status, file=log_file)
+
+        # logging needed when run as part of RMS, or when the argument is set
+        # NOTE: When run as part of RMS, the program name is "ExternalScript".
+        # Otherwise the program name is "__main__"
+        if __name__ == "__main__" and log_upload:
+            initLogging(config, "NM_UPLOAD_")
+            # Get the logger handle. 
+            log = logging.getLogger("logger.ExternalScript")
+        else:
+            log = logging.getLogger("logger")
+
+                    
+        # Upload files to the NM Server
+        getFilesAndUpload(log, nm_config, main_data_dir, log_file)
+
+        # Test for existence of "Extra_Uploads.sh".
+        # Execute it if it exists.
+
+        if (os.path.exists(extra_uploads_file)):
+            status = subprocess.call(extra_uploads_file, \
+                                     stdout=log_file, \
+                                     stderr=log_file, \
+                                     shell=True)
+            print(extra_uploads_file, " executed with status ", \
+                  status, file=log_file)
+        else:
+            print("No ", extra_uploads_file, " found to execute", \
+                  file=log_file)
+
+    # Reboot the Pi if requested. Code stolen from StartCapture.py.
+    # (script needs sudo priviledges, works only on Linux)
+
+    if __name__ == "__main__" and log_upload:
+        initLogging(config, "NM_UPLOAD_")
+
+    if reboot:
+        log.info('Rebooting now!')
+        try:
+            os.system('sudo shutdown -r now')
+
+        except Exception as e:
+            log.debug('Rebooting failed with message:\n' + repr(e))
+            log.debug(repr(traceback.format_exception(*sys.exc_info())))
+
+########################################################################
+
+if __name__ == "__main__":
+
+    
+    nm_parser = argparse.ArgumentParser(description="""Upload files to New_Mexico_Server, and optionally move other files to storage devices, create a TimeLapse.mp4 file, and reboot the system after all processing.""")
+    
+    nm_parser.add_argument('--directory', type=str, \
+                           help="Subdirectory of CapturedFiles or ArchiveFiles to upload. For example, US0006_20190421_020833_566122")
+    nm_parser.add_argument('--reboot', type=int, choices=[0,1], default=1,\
+                           help="When 1, reboot at end of ExternalScript. 0 Prevents reboot. Default is 1.")
+    nm_parser.add_argument('--CreateTimeLapse', type=int, choices=[0, 1], default=1,\
+                           help="When 1, create the TimeLapse.mp4 file. 0 prevents creation. Default is 1")
+    nm_parser.add_argument('--CreateCaptureStack', type=int, choices=[0, 1], default=1,\
+                           help="When 1, create the stack of all Captures in a JPEG file. 0 prevents creation. Default is 1")
+    nm_parser.add_argument('--which', type=str, choices=['C','A'], default='A',\
+                           help="C for CapturedFiles, A for ArchivedFiles. A is default")
+    nm_parser.add_argument('--preset', type=str, default='micro', \
+                           choices=['full', 'minimal', 'micro', 'imgs'], \
+                           help="which fileset to upload")
+    args = nm_parser.parse_args()
+
+    if args.directory == None:
+        print ("Directory argument not present! Exiting ...")
+        sys.exit()
+ 
+    print ('directory arg: ', args.directory)
+    print ('Reboot arg: ', args.reboot)
+    print ('CreateTimeLapse arg: ', args.CreateTimeLapse)
+    print ('CreateCaptureStack arg: ', args.CreateCaptureStack)
+    print ('which arg: ', args.which)
+    print ('preset arg: ', args.preset)
+
+   
+    config = RMS.ConfigReader.loadConfigFromDirectory(None, "/home/pi/source/RMS/.config")
+
+    print("config.data_dir = ", config.data_dir)
+
+    captured_data_dir = os.path.join(config.data_dir, 'CapturedFiles', args.directory)
+    archive_data_dir = os.path.join(config.data_dir, 'ArchivedFiles', args.directory)
+    
+    uploadFiles(captured_data_dir, archive_data_dir, config, log_upload=True, \
+                reboot=args.reboot, CreateTimeLapse=args.CreateTimeLapse, \
+                preset='micro')
+
+
+
+    
---
+++ TimeLapse.sh
@@ -0,0 +1,260 @@
+#!/bin/bash
+
+# TimeLapse.sh  script, see printf below for rev date
+#
+# Create timelapse file, then move it, .csv, & *radiants.txt files 
+#  so they can be easily located.
+# Argument1 ($1): ArchivedFiles directory name
+# Argument2 ($2): False, 0 or No will skip creating TimeLapse.mp4
+# Argument3 ($3): False, 0 or No will skip creating stack of CapturedFiles
+# Default script action is to create TimeLapse.mp4 and Capture stack
+
+printf 'TimeLapse.sh, revised 05-Feb-2021, was called with\n'
+printf 'Arg 1 = %s, arg 2 = %s, arg 3 = %s\n\n' "$1" "$2" "$3"
+printf 'TimeLapse.sh copies radiants.txt, and .csv files to RMS_data/csv/,\n'
+printf 'then creates a TimeLapse.mp4 file, and stack of all captured images,\n'
+printf 'which are moved to RMS_data. You can suppress creation of the mp4\n'
+printf 'and captured stack by specifying  False, 0 or No  as the second\n' 
+printf 'argument for mp4 and third argument for captured stack.\n'
+printf 'TimeLapse.mp4, Radiants, Stack and Capture jpg files can optionally\n'
+printf 'be copied to My_Uploads by setting My_Uploads=1 in this script file.\n'
+printf 'You can change the default actions of this script by editing the\n'
+printf 'values for TimeLapse and CapStack in this script file.\n\n'
+
+# Use the following three vars to control script default actions: 
+#  0 for no, 1 for yes
+TimeLapse=1
+CapStack=1
+My_Uploads=1
+
+# Let's check that second argument.
+if [[ "$2" = "False" || "$2" = "0" || "$2" = "No" ]] ; then
+    TimeLapse=0
+fi
+
+# Let's check that third argument.
+if [[ "$3" = "False" || "$3" = "0" || "$3" = "No" ]] ; then 
+    CapStack=0
+fi
+
+archive_dir="/home/pi/RMS_data/ArchivedFiles"
+capture_dir="/home/pi/RMS_data/CapturedFiles"
+data_dir="/home/pi/RMS_data"
+
+# Sanity checks
+if [[ ! -d $archive_dir ]]
+then
+    echo "Directory $archive_dir does not exist! Exiting ..." >&2
+    exit 1
+fi
+
+if [[ ! -d $capture_dir ]]
+then
+    echo "Warning: Directory $capture_dir does not exist! Exiting ..." >&2
+    exit 1
+fi
+
+if [[ ! -d $data_dir ]]
+then
+    echo  "Directory $data_dir does not exist! Exiting ..." >&2
+    exit 1
+fi
+# End Sanity Checks
+
+# Let's check that first argument. It must be an ArchivedFiles directory.
+if [[ $1 = '' || ! -d "${archive_dir}"/$1 ]] ;
+then
+    printf 'Argument %s must specify a first-level sub-directory of %s\n' \
+        "$1" ${archive_dir}
+    exit 1
+fi
+
+# The substring operator in the following line grabs the station name
+# from the directory name
+station_name=${1:0:6}
+OUTFILE="/home/pi/RMS_data/csv/"${station_name}_"fits_counts.txt"
+echo "Station_name is ${station_name}"
+
+# Get the environment for Python set up, and move to the right directory
+source /home/pi/vRMS/bin/activate
+cd /home/pi/source/RMS/
+
+if [[ ${My_Uploads} = 1 ]]; then
+    printf 'Cleaning out older files in My_Uploads\n'
+    rm /home/pi/RMS_data/My_Uploads/*
+fi
+
+# Create the timelapse
+if [[ $TimeLapse = 0 ]] ; then
+    printf "\nSkipping creation of Timelapse mp4 for directory %s/%s\n" \
+        "${capture_dir}" "$1"
+else
+    if [[ -d "${capture_dir}/$1" ]] ; then 
+        printf '%s\n' "Creating Timelapse of directory ${capture_dir}/$1" 
+        python -m Utils.GenerateTimelapse /home/pi/RMS_data/CapturedFiles/"$1"
+        # Move the timelapse up to the RMS_data directory
+        printf 'Moving Timelapse to RMS_data\n'
+        cd -- $capture_dir/"$1"
+        if [[ ${My_Uploads} = 1 ]]; then
+            printf 'Copying TimeLapse.mp4 to My_Uploads\n'
+            cp ./*.mp4 /home/pi/RMS_data/My_Uploads/TimeLapse.mp4
+        fi
+        mv -v -- ./*.mp4 ${data_dir}
+    else
+	printf 'Directory %s does not exist. TimeLapse will not be created.\n' \
+	       "${capture_dir}/$1"
+    fi
+fi
+
+# Create a stack of the capture directory
+if [[ $CapStack = 0 ]] ; then
+    printf "\nSkipping creation of stack of CapturedFiles for directory %s/%s\n" \
+          "${capture_dir}" "$1"
+else
+    if [[ -d "${capture_dir}/$1" ]]
+    then 
+	# save capture stack
+	cd -- $capture_dir/"$1"
+	d_stack=$(ls ./*stack*_meteors.jpg)
+	mv ./*_meteors.jpg m
+
+	cd /home/pi/source/RMS/
+	printf "Creating a stack of the capture directory: %s\n" "$1"
+	python -m Utils.StackFFs /home/pi/RMS_data/CapturedFiles/"$1" jpg -s -x
+
+	cd -- $capture_dir/"$1"
+	c_stack=$(ls ./*stack*_meteors.jpg)
+	c_stack=${c_stack/meteors/captured}
+	mv ./*_meteors.jpg "$c_stack"
+
+	# restore detction stack
+	mv m "$d_stack"
+
+	# Check to see if this capture stack should be moved
+	if [[ ${My_Uploads} = 1 ]]
+	then
+            printf 'Copying Captured stack to My_Uploads\n'
+            cp ./*captured.jpg /home/pi/RMS_data/My_Uploads/Captured.jpg
+	fi
+	cp ./*captured.jpg "${data_dir}"
+	cp ./*captured.jpg "${archive_dir}/$1"
+	
+    else
+	printf 'Directory %s does not exist. Captured stack will not be created.\n' \
+	   "${capture_dir}/$1"
+    fi
+fi
+    
+cd -- $archive_dir/"$1"
+# Check for existence of the ~/RMS_data/csv directory
+if [[ ! -d "${data_dir}"/csv ]] ; then
+    printf 'Directory %s does not exist\n' "${data_dir}/csv"
+    if [[ -f "${data_dir}"/csv ]] ; then
+	# There is no directory called csv under RMS_data but there is a file
+	printf 'Moving file %s to file %s\n' "${data_dir}"/csv "${data_dir}"/csv-temp
+	mv "${data_dir}"/csv "${data_dir}"/csv-temp
+    fi
+    # No regular file with the name exists either
+    printf 'Creating directory %s\n' "${data_dir}"/csv
+    mkdir "${data_dir}"/csv
+    echo 'Number of fits files in Capture directory' > "$OUTFILE"
+else
+    # There already is a directory ~/RMS_data/csv
+    printf '\nDirectory %s already exists\n' "${data_dir}/csv"
+fi
+
+# Copy the csv file up to the RMS_data/csv directory
+printf 'Copying csv file to directory %s\n' "${data_dir}/csv"
+cp -v -- ./*.csv "${data_dir}/csv"
+
+# Copy the radiants.txt file up to the RMS_data/csv directory
+printf 'Copying radiants.txt file to directory %s\n' "${data_dir}/csv"
+cp -v -- ./*radiants.txt "${data_dir}/csv"
+
+if [[ ${My_Uploads} = 1 ]]; then
+    printf 'Copying Radiants.jpg to My_Uploads\n'
+    cp ./*radiants.png /home/pi/RMS_data/My_Uploads/Radiants.png
+fi
+
+# Collect information for output to csv file.
+# First, the number of FITS files
+
+fits_count="$(find "/home/pi/RMS_data/CapturedFiles/$1"/*.fits -type f  -printf x | wc -c)"
+
+printf "\n" 
+printf 'Number of fits files in Capture directory: %d\n' "$fits_count"
+
+# Find the number of detections in the stack file
+stack=$(ls "/home/pi/RMS_data/ArchivedFiles/$1"/*meteors.jpg)
+# Anything such file found?
+if [[ -n $stack ]]
+then
+    # Yes! Find the number of meteors encoded in the file name
+    detected=$(echo "$stack" | grep -Eo '[[:digit:]]+_meteors' | grep -Eo '[[:digit:]]+')
+else
+    # No such file found. No detections.
+    detected=0
+fi
+
+if [[ ${My_Uploads} = 1 ]]; then
+    printf 'Copying Detected Stack.jpg to My_Uploads\n'
+    cp ./*meteors.jpg /home/pi/RMS_data/My_Uploads/Stack.jpg
+fi
+
+# Count the number of directories under CapturedFiles and ArchivedFiles
+# The variable "date" extracts the date (in the form yyyymmdd)
+# from the first positional parameter (the directory name).
+# It uses substring syntax (take the substring starting at position 7
+# and extending 8 characters).
+# The variable "pattern" uses this date in a pattern that begins with "d"
+# which is the first character in a directory listing given by "ls -l".
+
+date=${1:7:8}
+printf "date: %s\n" "$date"
+pattern="^d.*${date}.*"
+printf 'pattern: %s\n' "$pattern"
+
+# CapturedFiles
+echo 'Directories under CapturedFiles:'
+ls -l /home/pi/RMS_data/CapturedFiles | grep "$pattern"
+
+captured_count=$(ls -l /home/pi/RMS_data/CapturedFiles | grep -c "$pattern")
+printf 'Directory count under CapturedFiles: %d\n' "$captured_count"
+
+# ArchivedFiles
+echo 'Directories under ArchivedFiles:'
+ls -l /home/pi/RMS_data/ArchivedFiles | grep "${pattern}"
+
+archived_count=$(ls -l /home/pi/RMS_data/ArchivedFiles | grep -c "$pattern")
+printf 'Directory count under ArchivedFiles: %d\n' "$archived_count"
+
+# Write it out to the file in the csv directory
+
+printf "%s: " "$1" >> "$OUTFILE"
+if [[ $captured_count -eq $archived_count ]]
+then
+   if [[ $archived_count -eq 1 ]]
+   then
+      printf "%d\t%d \n" >> "$OUTFILE" \
+        "$fits_count" "$detected"
+   else
+      printf "%d\t%d\t Arch_Dir: %d\n" >> "$OUTFILE" \
+        "$fits_count" "$detected" "$archived_count"
+   fi
+else
+  printf "%d\t%d\tCap_Dir: %d\tArch_Dir: %d\n" >> "$OUTFILE" \
+        "$fits_count" "$detected" "$captured_count" "$archived_count"
+fi
+
+# Now check for the TOTAL number of files in the CapturedFiles directory,
+# not just the number matching the date
+
+total_captured=$(ls -l /home/pi/RMS_data/CapturedFiles | grep -c "$station_name")
+
+if [[ $total_captured -le 1 ]]
+then
+    printf "WARNING: only %d directory in CapturedFiles\n" \
+	   "$captured_count" >> "$OUTFILE"
+fi
+
+printf "fits file count and number of detections saved to: %s\n" "$OUTFILE"
