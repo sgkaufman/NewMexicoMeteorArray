a71fbb77eb72706bf5ce39ef5af23ca3a1c0ca4f
Peter Eschman <eschman@unm.edu>
2021-02-24 12:44:21 -0700

    Adding five new scripts

---
+++ BackupToUSB.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# BackupToUSB.sh  script revised: 05-Feb, 2021
+# PNE: Added backup of captured stack image
+# PNE: Automate backups to thumbdrive: 12/05/2019
+# takes one arg, which is the <stationID>_Date_Time1_Time2 directory name
+# of the ArchivedFiles directory.
+# assumes that the destination has directories bz2, csv, CapStack, and TimeLapse
+# Argument1: ArchivedFiles directory name
+
+archive_dir="/home/pi/RMS_data/ArchivedFiles"
+data_dir="/home/pi/RMS_data"
+
+# Let's check that first argument. Must be in the ArchivedFiles directory.
+if [[ $1 = '' || ! -d "${archive_dir}"/$1 ]] ;
+then
+    printf 'Argument %s must specify a first-level sub-directory of %s\n' \
+        "$1" ${archive_dir} 
+    exit 1
+fi
+
+# set station specific USB drive designation
+USB_drive="/media/pi/US0002B_BK/US0002"
+station="US0002"
+
+# copy fits_count.txt
+target="${data_dir}/csv/${station}_fits_counts.txt"
+printf "Copying %s\n" "${target}"
+cp "${target}" "${USB_drive}/csv"
+
+# copy .csv
+target="${data_dir}/csv/$1.csv"
+printf "Copying %s\n" "${target}"
+cp "${target}" "${USB_drive}/csv"
+
+# copy radiants.txt
+target="${data_dir}/csv/$1_radiants.txt"
+printf "Copying %s\n" "${target}"
+cp "${target}" "${USB_drive}/csv"
+
+# copy tar.bz2
+target="${archive_dir}/$1_detected.tar.bz2"
+printf "Copying %s...\n" "${target}"
+cp "${target}" "${USB_drive}/bz2"
+
+# move TimeLapse.mp4
+target="${data_dir}/$1.mp4"
+printf "Moving %s\n" "${target}"
+mv "${target}" "${USB_drive}/TimeLapse"
+
+# move Captured_Stack.jpg
+target="${data_dir}/$1*_captured.jpg"
+printf "Moving %s\n" "${target}"
+mv /home/pi/Desktop/RMS_data/US*.jpg "${USB_drive}/CapStack"
+
+printf "Done copying data to USB drive %s\n " "${USB_drive}"
---
+++ FixIt.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# FixIt.sh	see echo line below for date of last update
+# Combine reprocessing & external script calls
+# typical call is:	./FixIt.sh US0008_20191002_011553_128819
+#
+# .FixIt.sh can take three arguments, all of which are 1 for Yes and 0 for No
+# .FixIt.sh <directory name> arg2 arg3 arg4 
+#   arg1 <directory name>
+#   arg2 reboot, where 0 overrides normal reboot
+#   arg3 timelapse, where 0 overrrides normal timelapse mp4 file creation
+#   arg4 reprocess data in ArchiveFilesReprocessed, where creation
+#	of timelapse mp4 using captured files directory is optional
+#
+# so for a normal run:	./FixIt.sh <directory name>
+#  no reboot: 		./FixIt.sh <directory name> 0
+#	since creation of timelapse is the default behavior, a mp4 will
+#	be created, resulting in the same behavior as the next example:
+#  no reboot, mp4	./FixIt.sh <directory name> 0 1
+#  reboot, no mp4: 	./FixIt.sh <directory name> 1 0
+#  no reboot, no mp4: 	./FixIt.sh <directory name> 0 0
+#  run on ArchivedFilesReprocessed:	./FixIt.sh <directory name> 0/1 0/1 1
+#					arg 2: 0 or 1
+#					arg 3: 0 or 1
+#					arg 4: 1 or other, only testing to see if arg4 exists
+# A typical run on ArchiveFilesReprocessed data, no reboot, and no mp4 wouild be:
+#			./FixIt.sh <directory name> 0 0 1
+
+cd /home/pi/source/RMS
+echo FixIt.sh, 22-Feb, 2021: Combining reprocess and external script calls...
+case $# in
+    "4")
+         echo "Reprocessing data in ArchivedFilesReprocessed $1, with reboot=$2, and create mp4=$3"
+         python -m RMS.Reprocess ~/RMS_data/ArchivedFilesReprocessed/"$1"
+         python -m RMS.ExternalScript --directory "$1" --reboot "$2" --CreateTimeLapse "$3"
+         ;;
+    "3")
+         echo "Reprocessing data in CapturedFiles $1, with reboot=$2, and create mp4=$3"
+         python -m RMS.Reprocess ~/RMS_data/CapturedFiles/"$1"
+         python -m RMS.ExternalScript --directory "$1" --reboot "$2" --CreateTimeLapse "$3"
+         ;;
+    "2")
+         echo "Reprocessing data in CapturedFiles $1, with reboot=$2, after mp4 created"
+         python -m RMS.Reprocess ~/RMS_data/CapturedFiles/"$1"
+         python -m RMS.ExternalScript --directory "$1" --reboot "$2"
+         ;;
+    *)
+         echo "Reprocessing data in CapturedFiles $1,  with mp4 created, then reboot"
+         python -m RMS.Reprocess ~/RMS_data/CapturedFiles/"$1"
+         python -m RMS.ExternalScript --directory "$1"
+         ;;
+esac 
