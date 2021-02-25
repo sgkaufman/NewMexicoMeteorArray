890e90a20151d19d4039ea255cbfac80ec726268
Stephen Kaufman <stephengkaufman@gmail.com>
2021-02-25 18:01:49 +0000

    Improved move_to_RMS.sh, deleted move_to_RMS_Python2.sh

--- move_to_RMS.sh
+++ move_to_RMS.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 # This script copies files in /home/pi/NMMA/NewMexicoMeteorArray
-# into /home/pi/source/RMS and its subdirectories, as appropriate.
+# into /home/pi/source/RMS and its subdirectories, as determined
+# by the Raspbian OS version specified in file /etc/os-release.
+
+# Sanity check
+if [[ ! -f /etc/os-release ]]; then
+    echo "File /etc/os-release does not exist, exiting ..."
+    exit 1
+fi
 
 git_dir="/home/pi/source/NMMA/NewMexicoMeteorArray"
 RMS_dir="/home/pi/source/RMS"
@@ -14,20 +21,24 @@ else
 fi
 
 
-var=$(cat /etc/os-release | grep 'buster')
+var=$( grep -o 'buster' /etc/os-release )
 if [[ -n "$var" ]]; then
-    printf "Copying TimeLapse.sh to %s\n" "$RMS_dir"
-    cp ./TimeLapse.sh /home/pi/source/RMS/TimeLapse.sh
-    printf "Copying ExternalScript.py to %s/RMS/ExternalScript.py\n" "$RMS_dir"
-    cp ./ExternalScript.py "$RMS_dir"/RMS/ExternalScript.py
-fi
 
-var=$(cat /etc/os-release | grep 'jessie')
-if [[ -n $var ]]; then
-    printf "Copying TimeLapse.sh to %s\n" "$RMS_dir"
-    cp ./TimeLapse.sh "$RMS_dir"/TimeLapse.sh
-    printf "Copying ExternalScript_Python2.py to %s/RMS/ExternalScript.py\n" \
+    case ${var:0:6} in
+	buster )
+	    printf "Copying TimeLapse.sh to %s\n" "$RMS_dir"
+	    cp ./TimeLapse.sh /home/pi/source/RMS/TimeLapse.sh
+	    printf "Copying ExternalScript.py to %s/RMS/ExternalScript.py\n" "$RMS_dir"
+	    cp ./ExternalScript.py "$RMS_dir"/RMS/ExternalScript.py
+	    ;;
+	jessie )
+	    printf "Copying TimeLapse.sh to %s\n" "$RMS_dir"
+	    cp ./TimeLapse.sh "$RMS_dir"/TimeLapse.sh
+	    printf "Copying ExternalScript_Python2.py to %s/RMS/ExternalScript.py\n" \
 	   "$RMS_dir"
-    cp ./ExternalScript_Python2.py "$RMS_dir"/RMS/ExternalScript.py
+	    cp ./ExternalScript_Python2.py "$RMS_dir"/RMS/ExternalScript.py
+	    ;;
+    esac
+else
+    echo "Raspbian OS neither buster nor jessie. Strange ..."
 fi
-
--- move_to_RMS_Python2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp ./TimeLapse.sh /home/pi/source/RMS/TimeLapse.sh
-cp ./ExternalScript_Python2.py /home/pi/source/RMS/RMS/ExternalScript.py
