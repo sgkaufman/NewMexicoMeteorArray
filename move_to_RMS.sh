daa1644719990989500fd5b098b984604e6f3b9f
Stephen Kaufman <stephengkaufman@gmail.com>
2021-02-27 01:15:01 +0000

    merge

--- move_to_RMS.sh
+++ move_to_RMS.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Version 0.2, 25-Feb-2021. Byte count = 1356
 # This script copies files in /home/pi/NMMA/NewMexicoMeteorArray
-# into /home/pi/source/RMS and its subdirectories, as appropriate.
+# into /home/pi/source/RMS and its subdirectories, as determined
+# by the Raspbian OS version specified in file /etc/os-release.
+
+# Sanity check
+if [[ ! -f /etc/os-release ]]; then
+    echo "File /etc/os-release does not exist, exiting ..."
+    exit 1
+fi
 
 git_dir="/home/pi/source/NMMA/NewMexicoMeteorArray"
 RMS_dir="/home/pi/source/RMS"
@@ -13,21 +21,24 @@ else
     cd "/home/pi/source/NMMA/NewMexicoMeteorArray"
 fi
 
-
-var=$(cat /etc/os-release | grep 'buster')
+var=$( grep -Eo 'buster|jessie' /etc/os-release )
 if [[ -n "$var" ]]; then
-    printf "Copying TimeLapse.sh to %s\n" "$RMS_dir"
-    cp ./TimeLapse.sh /home/pi/source/RMS/TimeLapse.sh
-    printf "Copying ExternalScript.py to %s/RMS/ExternalScript.py\n" "$RMS_dir"
-    cp ./ExternalScript.py "$RMS_dir"/RMS/ExternalScript.py
-fi
-
-var=$(cat /etc/os-release | grep 'jessie')
-if [[ -n $var ]]; then
-    printf "Copying TimeLapse.sh to %s\n" "$RMS_dir"
-    cp ./TimeLapse.sh "$RMS_dir"/TimeLapse.sh
-    printf "Copying ExternalScript_Python2.py to %s/RMS/ExternalScript.py\n" \
+    printf "grepped the following: %s\n" "$var"
+    case ${var:0:6} in
+	buster )
+	    printf "Copying TimeLapse.sh to %s\n" "$RMS_dir"
+	    cp ./TimeLapse.sh /home/pi/source/RMS/TimeLapse.sh
+	    printf "Copying ExternalScript.py to %s/RMS/ExternalScript.py\n" "$RMS_dir"
+	    cp ./ExternalScript.py "$RMS_dir"/RMS/ExternalScript.py
+	    ;;
+	jessie )
+	    printf "Copying TimeLapse.sh to %s\n" "$RMS_dir"
+	    cp ./TimeLapse.sh "$RMS_dir"/TimeLapse.sh
+	    printf "Copying ExternalScript_Python2.py to %s/RMS/ExternalScript.py\n" \
 	   "$RMS_dir"
-    cp ./ExternalScript_Python2.py "$RMS_dir"/RMS/ExternalScript.py
+	    cp ./ExternalScript_Python2.py "$RMS_dir"/RMS/ExternalScript.py
+	    ;;
+    esac
+else
+    echo "Raspbian OS neither buster nor jessie. Strange ..."
 fi
-
