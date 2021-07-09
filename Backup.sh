14afb8060081aee38deeb56f1a3f8cbba5f3c2d8
Stephen Kaufman <stephengkaufman@gmail.com>
2021-07-09 22:07:53 +0000

    Change destination ariable to original value

--- Backup.sh
+++ Backup.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 # Backup.sh
 # script backs up key files so microSD can be swapped
+# User is responsible for changing the variable "destination"
+# to the name of their own USB drive.
 
-printf "Backup.sh 09-Jul, 2021, byte count ~1015 : backs up files prior to microSD swap\n"
+printf "Backup.sh 09-Jul, 2021, byte count ~1070 : backs up files prior to microSD swap\n"
 
-destination="/media/pi/D25B-C884/US000N_Backup/Swap_MicroSD"
+destination="/media/pi/32GB_F32_2/Swap_MicroSD"
+
 mkdir ${destination}
 mkdir ${destination}/source
 mkdir ${destination}/source/RMS
