2161e791fd8c7bac869cf58791a51445d67d8661
Peter Eschman <eschman@unm.edu>
2021-07-14 15:37:46 -0600

    Revised RMS_Restart.sh to use two new pieces

--- RMS_Restart.sh
+++ RMS_Restart.sh
@@ -2,13 +2,16 @@
 # RMS_Restart.sh
 # Runs RMS_Update, then RMS_StartCatpure
 
-printf "RMS_Restart.sh 13-Jul, 2021, byte count 388 : Runs RMS_Update, then RMS_StartCatpure\n"
+printf "RMS_Restart.sh 14-Jul, 2021, byte count 388 : Runs RMS_Update, CaptureTimes, then RMS_StartCatpure\n"
 
 source vRMS/bin/activate
 cd source/RMS
 
 printf "\nRunning RMS_Update...\n"
 ./Scripts/RMS_Update.sh
+
+printf "\nRunning CaptureTimes...\n"
+./CaptureTimes.sh
 
 printf "\nRunning RMS_StartCatpure...\n"
 ./Scripts/RMS_StartCapture.sh
