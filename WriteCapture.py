16c13ffe6ee99b51f6940c292d6c49dae5f4be89
Stephen Kaufman <stephengkaufman@gmail.com>
2021-07-22 23:24:40 +0000

    Clean up comments and code

--- WriteCapture.py
+++ WriteCapture.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
 """
-Version of 22-Jul-2021; Bytes: 3978
+Version of 22-Jul-2021; Bytes: 3327
 Version 0.1, SGK, 6/28/2020. This file belongs in directory
 $HOME/source/RMS/RMS/.
 This file calls the RMS function captureDuration (from RMS.CaptureDuration)
-and writes it to a log file for use by the shell script RecordWatchdog.sh.
-The log file is named CaptureTimes_yyyy_mm_dd, and is put into directory
-~/RMS_data/logs/, and writes the start time in ISO format, and capture time
-as an integer, rounded from the floating point value retutrn by captureDuration.
+and writes the capture start time and capture duration to a log file 
+called "CaptureTime.log". It is used by the shell script RecordWatchdog.sh
+and other utilities.
+The log file is put into directory ~/RMS_data/logs/, 
+and writes the start time in ISO format, and capture time as an integer, 
+rounded from the floating point value returned by captureDuration.
 """
 from __future__ import print_function
 import os
@@ -16,27 +18,6 @@ import datetime
 import argparse
 import ephem
 from RMS.CaptureDuration import captureDuration
-
-def makeLogFile(log_file_dir, prefix, time_arg=None):
-    
-    # Create a log file to provide as stdout and stderr for
-    # the subprocess.calls of the shell scripts.
-    # Return the file pointer to the log file in OPEN state.
-    # time_arg, if given, is a datetime object.
-
-    if time_arg is None:
-        time_to_use = datetime.datetime.utcnow()
-    else:
-        time_to_use = time_arg
-
-    print( 'time_to_use in making CaptureTimes file: %s\n' % time_to_use)
-
-    log_filename = prefix + "_" + \
-        time_to_use.strftime( '%Y_%m_%d' ) + ".log"
-
-    full_filename = log_file_dir + log_filename
-    print("Creating log file name %s\n" % full_filename)
-    return full_filename
 
 ########################################################################
 
@@ -58,7 +39,7 @@ if __name__ == "__main__":
     print ("WriteCapture.py, Longitude: ", lon)
     print ("WriteCapture.py, Elevation: ", elev)
     
-    # Compute and write out the next start time and capture timedatetime.
+    # Compute and write out the next start time and capture time datetime.
     start_time, duration = captureDuration(lat, lon, elev)
     if (start_time == True):
         # We will use the ephemeris code directly
