75ada4bdaa61bbbc4fc4d60bcfe7f01dae16d536
Stephen Kaufman <stephengkaufman@gmail.com>
2022-03-03 00:41:46 +0000

    Add diagnostics

--- CallES.sh
+++ CallES.sh
@@ -7,7 +7,7 @@
 # 4. BackupToUSB.sh is called;
 # 5. My_Uploads file is called, if it exists;
 # 6. The usual file set is uploaded to NM Server.
-# Initially written by SGK, 01-Mar-2022. Byte Count = 739.
+# Initially written by SGK, 02-Mar-2022. Byte Count = 820.
 
 cd "${HOME}"/source/RMS
 source "${HOME}"/vRMS/bin/activate
@@ -17,7 +17,9 @@ host=$(hostname)
 date_str=$(date +%Y%m%d)
 dir=$(find "${data_dir}"/ArchivedFiles/ -type d | grep -o "${host}"_"${date_str}"[_0-9]*)
 
-env printf "%s\n" "${dir}"
+env printf "hostname: %s\n" "$host"
+env printf "date_str: %s\n" "$date_str"
+env printf "dir: %s\n" "${dir}"
 
 python -m RMS.ExternalScript --directory "${dir}" --CreateTimeLapse false \
        --CreateCaptureStack false --reboot false
